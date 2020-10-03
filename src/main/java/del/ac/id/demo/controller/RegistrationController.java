0413d5fdc22b5d4c423d28bb816c8e0ea9a50808
Chatrine <chatrine.manurung2001@gmail.com>
2020-10-03 09:32:26 +0700

    Update happy-travelling

--- src/main/java/del/ac/id/demo/controller/RegistrationController.java
+++ src/main/java/del/ac/id/demo/controller/RegistrationController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import del.ac.id.demo.jpa.Role;
 import del.ac.id.demo.jpa.RoleRepository;
