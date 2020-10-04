58f6078a6ee85f40765718452d44239042e70699
SariUliHutahaean <sariulihutahaean@gmail.com>
2020-10-04 16:19:18 +0700

    update app.properties

--- src/main/java/del/ac/id/demo/jpa/UserRepository.java
+++ src/main/java/del/ac/id/demo/jpa/UserRepository.java
@@ -2,6 +2,8 @@ package del.ac.id.demo.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, String> {
-	User findByUsername(String username);
+	    User findByUsername(String username);
+
 }
